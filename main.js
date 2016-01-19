218c50444472c810983a07025653788d32c33855
Mehgan Cook <mehgancook@yahoo.com>
2016-01-19 00:10:59 -0800

    dsf

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ Background.prototype.draw = function (ctx) {
 
 function Unicorn(game) {
  //   this.animation = new Animation(ASSET_MANAGER.getAsset("./img/Simba.png"), 0, 710, 59, 50, 0.02, 13, true, true);
-    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/Dino.png"), 0, 113, 150, 70, 0.95, 5, true, true);
+    this.animation = new Animation(ASSET_MANAGER.getAsset("./img/Dino.png"), 0, 113, 160, 70, 0.95, 5, true, true);
  //   this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 618, 334, 174, 138, 0.02, 40, false, true);
     this.jumping = false;
     this.radius = 100;
